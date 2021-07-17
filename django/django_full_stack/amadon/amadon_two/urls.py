4ede4ee6d7ac98e19c56476f37e053d4357fe944
franchango <40879079+franchango@users.noreply.github.com>
2021-07-16 19:25:45 -0500

    amadon
    deber actualizado

--- django/django_full_stack/amadon/amadon_two/urls.py
+++ django/django_full_stack/amadon/amadon_two/urls.py
@@ -1,4 +1,4 @@
-"""dashapp URL Configuration
+"""amadon_two URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,12 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
-
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.index)
-    
+    path('', include('poorly_coded_store.urls'))
 ]
